338337d3c2dd2190662086a448f57a8593aad489
Gheww <82707099+Gheww@users.noreply.github.com>
2021-04-17 14:04:54 +0300

    Create karesinibulma.cpp

---
+++ karesinibulma.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int sayi1,sonuc;
+    cout<<"Karesini Bulmak istediginiz Sayiyi Giriniz="<<endl;
+    cin >>sayi1;
+    sonuc = sqrt(sayi1);
+    cout<<"Sonuc="<<sonuc<<endl;
+    system("pause");
+    return 0;
+}
