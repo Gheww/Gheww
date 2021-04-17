5f20cb716935a3676a3345e53dd6b384d295f47b
Gheww <82707099+Gheww@users.noreply.github.com>
2021-04-17 14:41:38 +0300

    Create incten cmye cevirme.cpp

---
+++ incten cmye cevirme.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double inc,sonuc,cm;
+    cout<<"Cm'ye Cevirmek istediginiz inc degerini giriniz : ";
+    cin >> inc;
+    sonuc = inc*2.54;
+    
+    cout<<"Sonuc : "<<sonuc<<endl;
+
+    
+    system("pause");
+    return 0;
+}
