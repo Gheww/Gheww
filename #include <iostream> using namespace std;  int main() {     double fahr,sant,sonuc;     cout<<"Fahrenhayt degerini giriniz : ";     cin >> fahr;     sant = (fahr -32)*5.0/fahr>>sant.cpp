ee99db704eafc86db8d1783afe09588425c6245f
Gheww <82707099+Gheww@users.noreply.github.com>
2021-04-17 14:50:03 +0300

    Create fahr>>sant.cpp

---
+++ #include <iostream> using namespace std;  int main() {     double fahr,sant,sonuc;     cout<<"Fahrenhayt degerini giriniz : ";     cin >> fahr;     sant = (fahr -32)*5.0/fahr>>sant.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double fahr,sant,sonuc;
+    cout<<"Fahrenhayt degerini giriniz : ";
+    cin >> fahr;
+    sant = (fahr -32)*5.0/9.0;
+
+    cout<< "Sonuc : "<<sant<<endl;
+    
+
+    
+    system("pause");
+    return 0;
+}
